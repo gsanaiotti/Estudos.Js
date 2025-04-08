532453d2d2a7a002d981f0b7e96782f7ed514622
gsanaiotti <guilherme.sanaiotti@hotmail.com>
2025-04-07 23:39:17 -0300

    Instrução básica sobre blocos de códigos.

---
+++ 02-bloco_de_codigo.js
@@ -0,0 +1,16 @@
+{
+    console.log("Olá, este é o seu bloco de código.");
+    console.log("Imprima a soma de 2 + 5.");
+    console.log(2 + 5);
+}
+
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
