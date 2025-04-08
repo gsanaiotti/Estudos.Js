c5d761dc26b2cec1c0b573c268c5ad0d04cbd166
gsanaiotti <guilherme.sanaiotti@hotmail.com>
2025-04-07 23:26:04 -0300

    Sentenças de código e comentários

--- 01-hello_world.js
+++ 01-hello_world.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+console.log("Hello World!\n"); // Utiliza-se '//' para comentar códigos.
+
+console.log("Bom dia!");
+console.log("Boa tarde!");
+console.log("Boa noite!");
+
+ /*
+* Esta é a estrutura
+* para comentar códigos
+* em diversas linhas:
+* "Docstrings"
+*/
